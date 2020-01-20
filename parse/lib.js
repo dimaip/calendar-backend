36602e1bccda12d7e2ac953f1f559a9eab9c21fe
Dmitri Pisarev <dimaip@gmail.com>
2020-01-20 12:46:43 +0300

    TASK: by default scrape only 7 days ahead

--- parse/lib.js
+++ parse/lib.js
@@ -4,7 +4,7 @@ module.exports.generateDatesArray = () => {
 	const dateStart = process.argv[2] ? new Date(process.argv[2]) : new Date();
 	const dateEnd = process.argv[3] ? new Date(process.argv[3]) : new Date(dateStart.valueOf() + 7 * 864e5);
 	const dates = [];
-	for (var date = dateStart; date.getTime() < dateEnd.getTime(); date = new Date(date.getTime() + (24 * 60 * 60 * 1000))) {
+	for (var date = dateStart; date.getTime() < dateEnd.getTime(); date = new Date(date.getTime() + (24 * 60 * 60 * 7))) {
 		dates.push(date);
 	}
 	return dates;
