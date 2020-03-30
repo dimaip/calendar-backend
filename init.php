289e659d50769043495eb3ca381c5599628384b0
Dmitri Pisarev <dimaip@gmail.com>
2020-03-30 18:43:02 +0300

    Come on

--- init.php
+++ init.php
@@ -2,7 +2,7 @@
 
 require_once 'vendor/autoload.php';
 
-if (isset($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_X_FORWARDED_HOST'], 'd.psmb.ru') === 0) {
+if (isset($_SERVER['HTTP_X_FORWARDED_HOST']) && strpos($_SERVER['HTTP_X_FORWARDED_HOST'], 'd.psmb.ru') === 0) {
   ini_set('display_errors', 0);
   Sentry\init(['dsn' => 'https://e5296954a22242bc85d59b9a36559c44@sentry.io/3629452']);
 }
