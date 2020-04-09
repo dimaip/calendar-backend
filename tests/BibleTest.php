e9e7e3309fee966fba3e86eb67273e306c45ea44
Andrey Zakharov <zaharov.aa@mail.ru>
2020-04-10 03:22:47 +0400

    #12 refactoring tests. added day tests

--- tests/BibleTest.php
+++ tests/BibleTest.php
@@ -1,25 +1,45 @@
 <?php
-require_once __DIR__.'/../init.php';
-require_once __DIR__.'/../bible.php';
+require_once __DIR__ . '/../init.php';
+require_once __DIR__ . '/TestIterator.php';
 
 use PHPUnit\Framework\TestCase;
 
 class BibleTest extends TestCase
 {
-    public function testReadings()
+    /**
+     * @dataProvider readingProvider
+     */
+    public function testReadings($reading, $translation, $expected)
     {
-        $data = json_decode(file_get_contents(__DIR__."/BibleTest.json"), true);
-        foreach ($data as $pair) {
-            $bible = new Bible;
-            $zachalo = $pair['reading']?? null;
-            $translation = $pair['translation'] ?? null;
-            if ($translation === 'default') {
-                $translation = null;
-            }
-            if ($zachalo) {
-                $result = $bible->run($zachalo, $translation);
-                $this->assertSame($pair['fragments'], $result['fragments']);
-            }
+        require_once __DIR__ . '/../bible.php';
+        $bible = new Bible;
+        $zachalo = $reading ?? null;
+        if ($translation === 'default') {
+            $translation = null;
         }
+
+        $result = $bible->run($zachalo, $translation);
+        $this->assertSame($expected, $result['fragments']);
+    }
+
+    public function readingProvider()
+    {
+        return new TestIterator(json_decode(file_get_contents(__DIR__ . "/BibleTest_readings.json"), true), ['reading', 'translation', 'fragments']);
+    }
+
+    /**
+     * @dataProvider dayProvider
+     */
+    public function testDay($date, $expected)
+    {
+        require_once __DIR__ . '/../day.php';
+        $day = new Day;
+        $result = $day->run(str_replace('-', '', $date));
+        $this->assertSame($expected, $result['readings']);
+    }
+
+    public function dayProvider()
+    {
+        return new TestIterator(json_decode(file_get_contents(__DIR__ . "/BibleTest_day.json"), true), ['date', 'readings']);
     }
 }
