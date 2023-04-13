8b85fe7c73ebf3ec44a95e08d053cbcf8b40b0f5
Dmitri Pisarev <dimaip@gmail.com>
2023-04-13 21:51:23 +0300

    Warmup Airtable on cache clear

--- clearCache.php
+++ clearCache.php
@@ -1,6 +1,11 @@
 <?php
 require_once('init.php');
+include __DIR__ . '/day.php';
 
 exec('rm -f ' . dirname(__FILE__) . '/Data/cache_*');
 exec('rm -f ' . dirname(__FILE__) . '/Data/cache/*');
+
+getPerehod();
+getNeperehod();
+
 echo "Caches cleared";
